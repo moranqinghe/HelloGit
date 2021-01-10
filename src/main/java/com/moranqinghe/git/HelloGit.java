4c127feccb454f6e015918b712333a822321ced5
moranqinghe <moran.qinghe520>
2021-01-10 20:06:00 +0800

    create branch1

--- src/main/java/com/moranqinghe/git/HelloGit.java
+++ src/main/java/com/moranqinghe/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("update1");
         System.out.println("update2");
+        System.out.println("create branch1");
     }
 }
